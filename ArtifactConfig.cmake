6fb066523f3edeaf67d263fee79078210c90dca6
Mr.Nobody <marek@embedbits.com>
2026-09-03 09:33:25 +0200

    Core/0.0.1

---
+++ ArtifactConfig.cmake
@@ -0,0 +1,46 @@
+set(NINJA_CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_DIR})
+#------------------------------------------------------------------------------#
+# Returns artifact version.
+#
+# The name of function must consist of folder name (ninja) and postfix 
+# (_GetArtifactVersion). Otherwise the buildprocess will fail.  
+#
+# ARTIFACT_VERSION [out]: Version of artifact in format X.Y.Z
+#------------------------------------------------------------------------------#
+function(ninja_GetArtifactVersion RET_VERSION)
+
+    execute_process(COMMAND ninja --version
+                    OUTPUT_VARIABLE ARTIFACT_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+                    
+    string(REGEX REPLACE ".*.([0-9]+).([0-9]+).([0-9]+).*" "\\1.\\2.\\3" VERSION "${ARTIFACT_VERSION}")
+                    
+    set(${RET_VERSION} "${VERSION}" PARENT_SCOPE)
+
+endfunction()
+
+
+#------------------------------------------------------------------------------#
+# Initialize artifact for build.
+#
+# The name of function must consist of folder name (ninja) and postfix 
+# (_ArtifactInstall). Otherwise the buildprocess will fail.  
+#
+# ARTIFACT_VERSION [out]: Version of artifact in format X.Y.Z
+#------------------------------------------------------------------------------#
+function(ninja_ArtifactInit)
+
+    set(CMAKE_GENERATOR "Ninja" CACHE INTERNAL "" FORCE)
+
+    #Set ninja path
+    if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+    
+        set(ENV{PATH} "${NINJA_CURRENT_LIST_DIR};$ENV{PATH}")
+        
+    else()
+        
+        set(ENV{PATH} "${NINJA_CURRENT_LIST_DIR}:$ENV{PATH}")
+        
+    endif()
+
+endfunction()
