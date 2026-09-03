9e22fc5fa4dc44588e021a83f2affa4127225af2
Mr.Nobody <marek@embedbits.com>
2026-09-03 12:39:02 +0200

    Core/1.0.0

--- ArtifactConfig.cmake
+++ ArtifactConfig.cmake
@@ -26,20 +26,20 @@ endfunction()
 # The name of function must consist of folder name (ninja) and postfix 
 # (_ArtifactInstall). Otherwise the buildprocess will fail.  
 #
-# ARTIFACT_VERSION [out]: Version of artifact in format X.Y.Z
+# ARTIFACT_BIN_PATH_ARG [in]: Path to the binary part of artifact
 #------------------------------------------------------------------------------#
-function(ninja_ArtifactInit)
+function(ninja_ArtifactInit ARTIFACT_BIN_PATH_ARG)
 
     set(CMAKE_GENERATOR "Ninja" CACHE INTERNAL "" FORCE)
 
     #Set ninja path
     if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
     
-        set(ENV{PATH} "${NINJA_CURRENT_LIST_DIR};$ENV{PATH}")
+        set(ENV{PATH} "${ARTIFACT_BIN_PATH_ARG};$ENV{PATH}")
         
     else()
         
-        set(ENV{PATH} "${NINJA_CURRENT_LIST_DIR}:$ENV{PATH}")
+        set(ENV{PATH} "${ARTIFACT_BIN_PATH_ARG}:$ENV{PATH}")
         
     endif()
 
